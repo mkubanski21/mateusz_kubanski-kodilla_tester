f08b068d7ead6a12b7c35cf84c7a959cc3786989
Mateusz Kubanski <mkubanski95@gmail.com>
2021-09-30 22:18:49 +0200

    MainExtendedUpdated

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String name = "Mateusz";
+        String city = "Krakow";
+        int number = 20;
+        double height = 180.5;
+        char group = 'A';
+        boolean isFromPoland = true;
+
+        System.out.println(name);
+        System.out.println(city);
+        System.out.println(number);
+        System.out.println(height);
+        System.out.println(group);
+        System.out.println(isFromPoland);
+    }
+}
