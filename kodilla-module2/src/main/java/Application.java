893b0e43ba8c0c2cd5336bcd0f88bfe19d793ead
Mateusz Kubanski <mkubanski95@gmail.com>
2021-10-18 21:43:17 +0200

    Module2.6_update

--- kodilla-module2/src/main/java/Application.java
+++ kodilla-module2/src/main/java/Application.java
@@ -10,29 +10,22 @@ public class Application {
     }
 
     public void checkAge() {
-        if (this.name != null) {
-            if (this.age > 50) {
-                System.out.println("User is older than 50.");
-            } else if (this.age > 30) {
-                System.out.println("User is older than 30 and equal or younger than 50.)");
-            } else {
-                System.out.println("User is equal and younger than 30.");
-            }
+        if (this.age > 50) {
+            System.out.println("User is older than 50.");
+        } else if (this.age > 30) {
+            System.out.println("User is older than 30 and equal or younger than 50.)");
         } else {
-            System.out.println("No name specified.");
+            System.out.println("User is equal and younger than 30.");
         }
     }
+
     public void checkHeight() {
-        if (this.name != null) {
-            if (this.height > 180) {
-                System.out.println("User is taller than 180 cm.");
-            } else if (this.height > 160) {
-                System.out.println("User is taller than 160 and equal or lower than 180 cm.");
-            } else {
-                System.out.println("User is equal and lower than 160 cm.");
-            }
+        if (this.height > 180) {
+            System.out.println("User is taller than 180 cm.");
+        } else if (this.height > 160) {
+            System.out.println("User is taller than 160 and equal or lower than 180 cm.");
         } else {
-            System.out.println("No name specified.");
+            System.out.println("User is equal and lower than 160 cm.");
         }
     }
 }
