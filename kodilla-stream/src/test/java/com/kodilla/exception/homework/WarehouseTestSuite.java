6e7080a999c4e889e26bb069cb61d65830e8d217
Mateusz Kubanski <mkubanski95@gmail.com>
2021-11-20 16:32:33 +0100

    Module6.5_update

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -1,6 +1,5 @@
 package com.kodilla.exception.homework;
 
-import com.kodilla.exception.AirportNotFoundException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
